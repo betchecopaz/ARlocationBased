b94f50889adc72000c9240cd231f887b92471dd3
betchecopaz <etchecopaz_bruno@redlink.com.ar>
2016-04-20 12:45:58 -0300

    primer intento

---
+++ arlocationdemo/config.inc.php
@@ -0,0 +1,6 @@
+<?php
+
+/* Pre-define connection to the MySQL database, please specify these fields based on your database configuration.*/
+  define('DBHOST', 'localhost');
+  define('DBDATA', 'ARdb');
+?>
---
+++ arlocationdemo/layardemo/config.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+/* Pre-define connection to the MySQL database, please specify these fields based on your database configuration.*/
+  define('DBHOST', '75.126.155.153');
+  define('DBDATA', 'ARdb');
+  define('DBUSER', 'user10883');
+  define('DBPASS', 'PR4dLP9kT5fE');
+ ?>
